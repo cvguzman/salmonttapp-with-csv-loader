dc6120a73f0148bec2da7b736574620663a35705
cvguzman <157186909+cvguzman@users.noreply.github.com>
2025-12-14 00:11:36 -0300

    en la clase GestorUnidades se modifican los objetos con la interfaz mostrarResumen()

--- src/main/java/org/cvguzman/data/GestorUnidades.java
+++ src/main/java/org/cvguzman/data/GestorUnidades.java
@@ -2,6 +2,7 @@ package org.cvguzman.data;
 
 import org.cvguzman.model.CentroCultivo;
 import org.cvguzman.model.PlantaProceso;
+import org.cvguzman.model.Registrable;
 import org.cvguzman.model.UnidadOperativa;
 
 import java.util.ArrayList;
@@ -10,23 +11,23 @@ import java.util.List;
 // Se crea clase para instancias de prueba
 public class GestorUnidades {
 
-    public List<UnidadOperativa>  cargarUnidades() {
+    public List<Registrable>  cargarUnidades() {
 
-        List<UnidadOperativa> unidades = new ArrayList<>();
+        List<Registrable> unidades = new ArrayList<>();
 
         // Se crean objetos para CentroCultivo
         CentroCultivo centro1 = new CentroCultivo("Centro Llanquihue", "Puerto Montt", 116);
-        centro1.mostrarInformacion();
+        centro1.mostrarResumen();
 
         CentroCultivo centro2 = new CentroCultivo("Centro Frutillar", "Puerto Montt", 322);
-        centro2.mostrarInformacion();
+        centro2.mostrarResumen();
 
         // Se crean objetos para PlantaProceso
         PlantaProceso planta1 = new PlantaProceso("Planta Pacific Blue", "Aysén", 3000);
-       planta1.mostrarInformacion();
+        planta1.mostrarResumen();
 
-       PlantaProceso planta2 = new PlantaProceso("Planta Aqua Chile", "Río Bueno", 2500);
-       planta2.mostrarInformacion();
+        PlantaProceso planta2 = new PlantaProceso("Planta Aqua Chile", "Río Bueno", 2500);
+        planta2.mostrarResumen();
 
         // Agregamos en la lista
         unidades.add(centro1);
