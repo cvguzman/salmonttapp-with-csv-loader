3136d2c353ca6ca8b11c47d5612ebaae7ec5466e
cvguzman <157186909+cvguzman@users.noreply.github.com>
2025-12-17 22:55:11 -0300

    Se mejora interfaz y toString()

--- src/main/java/org/cvguzman/model/Colaborador.java
+++ src/main/java/org/cvguzman/model/Colaborador.java
@@ -1,27 +1,32 @@
 package org.cvguzman.model;
 
-public class Colaborador extends UnidadOperativa implements Registrable {
+public class Colaborador extends UnidadOperativa {
 
     private String colaborador;
+    private int telefono;
 
-    public Colaborador(String colaborador, String nombre, String comuna, int valor) {
+    public Colaborador(String nombre, String comuna, String colaborador, int telefono) {
         super(nombre, comuna);
         this.colaborador = colaborador;
+        this.telefono = telefono;
     }
 
     public String getColaborador() {
         return colaborador;
     }
 
-    public Object mostrarResumen() {
-        System.out.println("El colaborador " + getColaborador() + " está a cargo del " + getNombre() + " en " + getComuna());
-        return "El colaborador " + getColaborador() + " está a cargo del " + getNombre() + " en " + getComuna();
+    @Override
+    public String mostrarResumen() {
+        return "El colaborador " + getColaborador()
+                + " está a cargo del Centro " + getNombre()
+                + " en " + getComuna();
     }
-
+@Override
     public String toString() {
-        return "UnidadOperativa{" +
-                "colaborador= '" + getColaborador() + '\'' +
-                "nombre= '" + getNombre() + '\'' +
+        return "Colaborador{" +
+                "colaborador= '" + colaborador + '\'' +
+                "teléfono= '" + telefono + '\'' +
+                "centro= '" + getNombre() + '\'' +
                 ", comuna= '" + getComuna() + '\'' +
                 '}';
     }
